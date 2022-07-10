bd99e435b44df58950a9de974d47b2eed102a444
JERRYFROMKENYA <jerryochieng178@gmail.com>
2022-07-10 14:56:35 +0300

    nothing much.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import Editcard from './components/Cards/CardItem/Editcard';
+//import Editcard from './components/Cards/CardItem/Editcard';
 import { BrowserRouter as Router, Route, Routes ,Link } from 'react-router-dom'
 import Home from './pages/Home/Home';
 import Shop from './pages/Shop/Shop';
 import Portfolio from './pages/Portfolio/Portfolio';
-
+//<Route path="/card/edit"  element={<Editcard />}/>
 function App() {
   return (
     <>
@@ -17,7 +17,7 @@ function App() {
       <Route path="/" exact element={<Home/>}/>
       <Route path="/portfolio"  element={<Portfolio/>}/>
       <Route path="/shop"  element={<Shop />}/>
-      <Route path="/card/edit"  element={<Editcard />}/>
+      
      </Routes>
     </Router>
  
